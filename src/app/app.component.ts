0c4a14d11ae00e09edc25647024affd05d19b61e
perrytran242 <ptran209@gmail.com>
2020-02-21 21:01:48 -0800

    typing logic i progress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,36 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  typingChallenge = 'Lorem ipsum dolor sit.';
+export class AppComponent implements OnInit {
+  typingChallenge = 'Lorem ipsum dolor sit';
   compareWord = '';
   counter = 0;
+  splitWord = this.returnSplitWord();
+  correct = 'green';
+  error = 'red';
 
-
+  ngOnInit() {
+    // console.log('letter:', this.returnSplitWord());
+  }
 
   onInputReceived(event) {
-    const splitWord = this.returnSplitWord();
-    console.log(splitWord);
-    const { data } = event;
-    this.compareWord += data;
 
-    if (data === ' ') {
-      if (this.compareWord.trim() === splitWord[this.counter].word) {
-        console.log('compare word:', this.compareWord);
-        this.counter++;
-        this.compareWord = '';
-      }
+    const { value } = event.target;
+    if (this.splitWord[this.counter].letter === value[this.counter]) {
+      this.splitWord[this.counter].check = true;
     }
+
+
+    this.counter++;
+    console.log(this.counter);
+    console.log(this.splitWord);
+
   }
 
   returnSplitWord() {
-    return this.typingChallenge.split(' ').map(val => {
-      return {word: val, check: false};
+    return this.typingChallenge.split('').map(val => {
+      return {letter: val, check: false};
     });
   }
 }
-
+// user clicks and shows value
